8908fea1d355794107169cd5097219744c3c911b
Tomek <vovovov@wp.pl>
2017-03-09 20:40:12 +0100

    dodanie wyszukiwania po roku i typie

--- app/src/main/java/com/example/rent/movieapp/ListingActivity.java
+++ app/src/main/java/com/example/rent/movieapp/ListingActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
@@ -12,6 +14,9 @@ import android.widget.ViewFlipper;
 import com.annimon.stream.Stream;
 import com.annimon.stream.function.Consumer;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 import nucleus.factory.RequiresPresenter;
@@ -22,23 +27,37 @@ import nucleus.view.NucleusAppCompatActivity;
 public class ListingActivity extends NucleusAppCompatActivity<ListingPresenter> {
 
     private static final String SEARCH_TITLE = "searchTitle";
+    private static final String SEARCH_YEAR = "searchYear";
+    private static final String SEARCH_TYPE = "searchType";
     private MoviesListAdapter adapter;
-    private ViewFlipper viewFlipper;
-    private ImageView noInternetImage;
-    private RecyclerView recyclerView;
+    public static final int NO_YEAR_SELECTED = -1;
+
+    //butter knife zastepuje reczne inicjowanie(binduje)
+    @BindView(R.id.view_flipper)
+    ViewFlipper viewFlipper;
+    @BindView(R.id.no_internet_view)
+    ImageView noInternetImage;
+    @BindView(R.id.recycler_view)
+    RecyclerView recyclerView;
+    @BindView(R.id.no_results)
+    FrameLayout noResults;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listing);
+
+        ButterKnife.bind(this);
+
         String title = getIntent().getStringExtra(SEARCH_TITLE);
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        int year = getIntent().getIntExtra(SEARCH_YEAR, NO_YEAR_SELECTED);
+        String type = getIntent().getStringExtra(SEARCH_TYPE);
         adapter = new MoviesListAdapter();
         recyclerView.setAdapter(adapter);
-        viewFlipper = (ViewFlipper) findViewById(R.id.view_flipper);
-        noInternetImage = (ImageView) findViewById(R.id.no_internet_view);
 
-        getPresenter().getDataAsync(title)
+        //typowa rx java z nucleusem
+        getPresenter().getDataAsync(title,year,type)
         .subscribeOn(Schedulers.io())
         .observeOn(AndroidSchedulers.mainThread())
         .subscribe(this::success, this::error);
@@ -46,18 +65,29 @@ public class ListingActivity extends NucleusAppCompatActivity<ListingPresenter> 
 
     }
 
+    //Butterknife przed adnotacje
+    @OnClick(R.id.no_internet_view)
+    public void onNoInternetImageView(View view){
+        Toast.makeText(this, "kliknąłeś no internet image view", Toast.LENGTH_SHORT).show();
+    }
+
     private void error(Throwable throwable) {
         viewFlipper.setDisplayedChild(viewFlipper.indexOfChild(noInternetImage));
     }
 
     private void success(SearchResult searchResult) {
+        if("false".equalsIgnoreCase(searchResult.getResponse())){
+            viewFlipper.setDisplayedChild(viewFlipper.indexOfChild(recyclerView));
+        }else{
         viewFlipper.setDisplayedChild(viewFlipper.indexOfChild(recyclerView));
         adapter.setItems(searchResult.getItems());
-    }
+    }}
 
-    public static Intent createIntent(Context context, String title) {
+    public static Intent createIntent(Context context, String title, int year, String type) {
         Intent intent = new Intent(context, ListingActivity.class);
         intent.putExtra(SEARCH_TITLE, title);
+        intent.putExtra(SEARCH_YEAR, year);
+        intent.putExtra(SEARCH_TYPE, type);
         return intent;
     }
 
--- app/src/main/java/com/example/rent/movieapp/SearchActivity.java
+++ app/src/main/java/com/example/rent/movieapp/SearchActivity.java
@@ -3,24 +3,82 @@ package com.example.rent.movieapp;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.NumberPicker;
+import android.widget.RadioGroup;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnCheckedChanged;
+import butterknife.OnClick;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private Map<Integer, String> apiKeysMap = new HashMap<Integer, String>() {{
+        put(R.id.radio_movies, "Filmy");
+        put(R.id.radio_series, "Seriale");
+        put(R.id.radio_games, "Gry");
+        put(R.id.radio_episodes, "Odcinki");
+    }};
+    @BindView(R.id.number_picker)
+    NumberPicker numberPicker;
+    @BindView(R.id.search_edit_text)
+    TextInputEditText editText;
+    @BindView(R.id.search_button)
+    ImageButton searchButton;
+    @BindView(R.id.year_checkbox)
+    CheckBox yearCheckBox;
+    @BindView(R.id.type_checkbox)
+    CheckBox typeCheckBox;
+
+    @BindView(R.id.radio_group)
+    RadioGroup radioGroup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        ButterKnife.bind(this);
 
-        TextInputEditText editText = (TextInputEditText) findViewById(R.id.search_edit_text);
-        ImageButton searchButton = (ImageButton) findViewById(R.id.search_button);
-        searchButton.setOnClickListener(v -> {
-            startActivity(ListingActivity.createIntent(SearchActivity.this, editText.getText().toString()));
-        });
+        Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        numberPicker.setMinValue(1950);
+        numberPicker.setMaxValue(year);
+        numberPicker.setValue(year);
+        numberPicker.setWrapSelectorWheel(true);
 
 
-
-
-
+//        searchButton.setOnClickListener(v -> {
+//            startActivity(ListingActivity.createIntent(SearchActivity.this, editText.getText().toString()));
+//        });
     }
+
+
+    @OnCheckedChanged(R.id.type_checkbox)
+    void onTypeCheckboxStateChange(CompoundButton buttonView, boolean isChecked) {
+        radioGroup.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+    }
+
+    @OnCheckedChanged(R.id.year_checkbox)
+    void onCheckboxStateChanged(CompoundButton buttonView, boolean isChecked) {
+        numberPicker.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+    }
+
+    @OnClick(R.id.search_button)
+    void onSearchButtonClick() {
+        int checkRadioId = radioGroup.getCheckedRadioButtonId();
+        String typeKey = typeCheckBox.isChecked() ? apiKeysMap.get(checkRadioId) : null;
+        int year = yearCheckBox.isChecked() ? numberPicker.getValue() : ListingActivity.NO_YEAR_SELECTED;
+        startActivity(ListingActivity.createIntent(SearchActivity.this, editText.getText().toString(), year, typeKey));
+    }
+
+
 }
